71bd77912d095772e9cb72b078b3b74cd3261946
Gabriel Angel <gabriel.angel@ias.com.co>
2021-02-19 17:13:42 -0500

    se crea el home y navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/navbar";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <div className="container p-4">
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); 
